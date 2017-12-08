7a963f6b821639e3cafb9a0c4426028d74849190
Yuri Ramazanov <yuri.ramazanov@mobexs.com>
2017-12-08 12:52:32 +0300

    Add Data model entities

---
+++ EasyTime/DAL/Entities/Job+CoreDataProperties.swift
@@ -0,0 +1,50 @@
+//
+//  Job+CoreDataProperties.swift
+//  EasyTime
+//
+//  Created by Yury Ramazanov on 08/12/2017.
+//  Copyright © 2017 Mobexs. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Job {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Job> {
+        return NSFetchRequest<Job>(entityName: "Job")
+    }
+
+    @NSManaged public var currency: String?
+    @NSManaged public var customerId: String?
+    @NSManaged public var date: NSDate?
+    @NSManaged public var information: String?
+    @NSManaged public var jobId: String?
+    @NSManaged public var members: String?
+    @NSManaged public var name: String?
+    @NSManaged public var number: String?
+    @NSManaged public var statusId: String?
+    @NSManaged public var typeId: String?
+    @NSManaged public var expenses: NSSet?
+    @NSManaged public var images: Files?
+
+}
+
+// MARK: Generated accessors for expenses
+extension Job {
+
+    @objc(addExpensesObject:)
+    @NSManaged public func addToExpenses(_ value: Expense)
+
+    @objc(removeExpensesObject:)
+    @NSManaged public func removeFromExpenses(_ value: Expense)
+
+    @objc(addExpenses:)
+    @NSManaged public func addToExpenses(_ values: NSSet)
+
+    @objc(removeExpenses:)
+    @NSManaged public func removeFromExpenses(_ values: NSSet)
+
+}
---
+++ EasyTime/DAL/Entities/Type+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Type+CoreDataProperties.swift
+//  EasyTime
+//
+//  Created by Yury Ramazanov on 08/12/2017.
+//  Copyright © 2017 Mobexs. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Type {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Type> {
+        return NSFetchRequest<Type>(entityName: "Type")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var type: Int32
+    @NSManaged public var typeId: String?
+
+}
