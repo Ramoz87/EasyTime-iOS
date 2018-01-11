d78e82d9ccaec998287f4706fceec6f102dbe340
Sergei Kvasov <sergei.kvasov@mobexs.com>
2018-01-11 17:13:42 +0300

    WIP Add custom number keyboard

---
+++ EasyTime/User Interface/Shared/View Controllers/NumberInputViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NumberInputViewController.swift
+//  EasyTime
+//
+//  Created by Mobexs on 1/11/18.
+//  Copyright © 2018 Mobexs. All rights reserved.
+//
+
+import UIKit
+
+class NumberInputViewController: UIInputViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
