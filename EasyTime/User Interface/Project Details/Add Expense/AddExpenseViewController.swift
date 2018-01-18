8a72819a20bcfc82d4783905e128d432ea7a17a6
Sergei Kvasov <sergei.kvasov@mobexs.com>
2018-01-18 17:21:57 +0300

    WIP Implement AddExpense screen

--- EasyTime/User Interface/Project Details/Add Expense/AddExpenseViewController.swift
+++ EasyTime/User Interface/Project Details/Add Expense/AddExpenseViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+fileprivate struct Constants {
+
+    static let titleText = NSLocalizedString("Expenses", comment: "")
+}
+
 class AddExpenseViewController: BaseViewController<AddExpenseViewModel> {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var lblExpenseType: UILabel!
+    @IBOutlet weak var lblCurrency: UILabel!
+    @IBOutlet weak var btnAdd: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = Constants.titleText
+        self.btnAdd.alignVertical()
+
+        let controller = NumberInputViewController()
+        self.tfValue.inputViewController = controller
+    }
+
+    //MARK: - Action handlers
+
+    @IBAction func didTapAddButton(sender: Any) {
+
     }
 }
