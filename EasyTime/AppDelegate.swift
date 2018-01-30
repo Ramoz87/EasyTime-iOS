d5db401aa969a6236d4f8cec9007951f6544f7e2
Yuri Ramazanov <yuri.ramazanov@mobexs.com>
2018-01-30 12:47:14 +0300

    Add Fabric

--- EasyTime/AppDelegate.swift
+++ EasyTime/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
+
         print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
         IQKeyboardManager.sharedManager().enable = true
         configureScreens();
