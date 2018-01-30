df895415b1370e66c6ccc0b98bea0c2104aab932
Sergei Kvasov <sergei.kvasov@mobexs.com>
2018-01-30 12:35:04 +0300

    Hide back button text

--- EasyTime/User Interface/Shared/View Controllers/BaseViewController.swift
+++ EasyTime/User Interface/Shared/View Controllers/BaseViewController.swift
@@ -26,4 +26,10 @@ class BaseViewController<T>: UIViewController where T: BaseViewModel {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
 }
