9134b90a7e60a1521cd44f4ddccf31d1898d2d22
Sergei Kvasov <sergei.kvasov@mobexs.com>
2018-02-07 16:44:07 +0300

    WIP Implement Tutorial screen

---
+++ EasyTime/User Interface/Tutorial/TutorialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialViewController.swift
+//  EasyTime
+//
+//  Created by Mobexs on 2/7/18.
+//  Copyright © 2018 Mobexs. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: BaseViewController<TutorialViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ EasyTime/User Interface/Tutorial/TutorialViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TutorialViewModel.swift
+//  EasyTime
+//
+//  Created by Mobexs on 2/7/18.
+//  Copyright © 2018 Mobexs. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewModel: BaseViewModel {
+
+}
