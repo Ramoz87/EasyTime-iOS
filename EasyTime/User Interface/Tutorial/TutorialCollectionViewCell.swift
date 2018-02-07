ae5ffa185a31a2f26c7f7da1704f06643aef2fd2
Sergei Kvasov <sergei.kvasov@mobexs.com>
2018-02-07 16:45:26 +0300

    WIP Implement Tutorial screen

---
+++ EasyTime/User Interface/Tutorial/TutorialCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialCollectionViewCell.swift
+//  EasyTime
+//
+//  Created by Mobexs on 2/7/18.
+//  Copyright © 2018 Mobexs. All rights reserved.
+//
+
+import UIKit
+
+class TutorialCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
